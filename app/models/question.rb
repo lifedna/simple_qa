fdc18fec8eadf750873a6568d0e3d4ee350d7b48
Alex N <alex@vinova.sg>
2011-05-02 14:35:06 +0800

    No need manually index relevant fields. voteable ..., :index => true will create indexes for you

--- app/models/question.rb
+++ app/models/question.rb
@@ -20,7 +20,7 @@ class Question
   
   delegate :email, :to => :user, :allow_nil => true, :prefix => true
   
-  voteable self, :up => +1, :down => -1
+  voteable self, :up => +1, :down => -1, :index => true
   
   def self.inc_counter(id, field, value)
     collection.update({ '_id' => id }, {
@@ -37,16 +37,7 @@ class Question
     count
   end
   
-  # Compound index _id and voters.up, _id and voters.down
-  # to make up_voted_by, down_voted_by, voted_by scopes and voting faster
-  index [['votes.up', 1], ['_id', 1]], :unique => true
-  index [['votes.down', 1], ['_id', 1]], :unique => true
-
-  # Index counters and point for desc ordering
-  index [['votes.count', -1]]
-  index [['votes.up_count', -1]]
-  index [['votes.down_count', -1]]
-  index [['votes.point', -1]]
+  # Index counter desc ordering
   index [['answers_count', -1]]
 
   scoped_order 'votes.count', 'votes.up_count', 'votes.down_count', 'votes.point', 'answers_count'
